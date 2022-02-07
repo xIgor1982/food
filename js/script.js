f02d8b7da323dc26bed8c6475a25c30a810c244d
Igor <i_swat@mail.ru>
2022-02-08 01:21:00 +0300

    43. Создаем модальное окно

--- js/script.js
+++ js/script.js
@@ -99,4 +99,38 @@ window.addEventListener('DOMContentLoaded', () => {
     };
 
     setClock('.timer', deadline);
+
+    // Modal
+
+    const modalTrigger = document.querySelectorAll('[data-modal]'),
+          modal = document.querySelector('.modal'),
+          modalCloseBtn = document.querySelector('[data-close]');
+
+        modalTrigger.forEach(btn => {
+            btn.addEventListener('click', () => {
+                modal.classList.add('show');
+                modal.classList.remove('hidden');
+                document.body.style.overflow = 'hidden';
+            });
+        });
+
+        const closeModal = () => {
+            modal.classList.add('hidden');
+            modal.classList.remove('show');
+            document.body.style.overflow = '';            
+        };
+
+        modalCloseBtn.addEventListener('click', closeModal);
+
+        modal.addEventListener('click', (e) => {
+            if(e.target === modal) {
+                closeModal();
+            }
+        });
+
+        document.addEventListener('keydown', (e) => {
+            if(e.code === 'Escape' && modal.classList.contains('show')) {
+                closeModal();
+            }
+        });
 });
